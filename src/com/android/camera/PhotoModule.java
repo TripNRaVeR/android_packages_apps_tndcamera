f3d70e2f0719c642291e09167c65c0298a9d1bec
Suman Mukherjee <sumam@codeaurora.org>
2013-12-17 09:42:53 +0530

    Camera: Enable module switcher on resume
    For HDR mode, on pressing shutter key, module switcher icon will be
    turned off. That'll not be activated if user relaunch camera
    after pressing home key in HDR mode while capturing image.
    
    Enable switcher in UI on resuming camera module.
    
    Change-Id: I094f4941f87149e4c5b2393881ed5175c4388559
    CRs-fixed: 580450

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -727,6 +727,9 @@ public class PhotoModule
             s.setListener(this);
         }
         mNamedImages = new NamedImages();
+        if (!mIsImageCaptureIntent) {
+            mUI.showSwitcher();
+        }
         mUI.initializeSecondTime(mParameters);
         keepMediaProviderInstance();
     }
